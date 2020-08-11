505ceab70d6848afa5cb882c87f856b2ac9b6a50
adastraz <tylergosox@gmail.com>
2020-08-11 17:28:01 -0400

    basic functionality in working order
    get user information along with following

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import PrivateRoute from './components/PrivateRoute.js'
 import history from './utils/history'
 import Welcome from './components/Welcome.js'
 import SignIn from './components/SignIn.js'
+import ListFollowers from './components/followers/ListFollowers.js'
+import Profile from './components/user/Profile.js'
 
 function App() {
   return (
     <Router history={history}>
       <div>
         <Switch>
+          <PrivateRoute path='/profile/:id' component={Profile} />
+          <PrivateRoute path='/followers' component={ListFollowers} />
           <Route exact path='/' component={Welcome} />
           <Route path='/signup' component={SignIn} />
           <Route path='/signin' component={SignIn} />
