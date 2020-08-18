414e86adf6e38fd4d76bfcdcaee9de8405f25445
adastraz <tylergosox@gmail.com>
2020-08-18 16:00:52 -0400

    attempting to make dynamic profile
    going to have open inputs if a section of their profile is set to null

--- src/components/user/Profile.js
+++ src/components/user/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { useParams, Link } from 'react-router-dom'
 import { fetchUser, getFollowing } from '../../actions'
@@ -6,17 +6,68 @@ import { fetchUser, getFollowing } from '../../actions'
 const Profile = props => {
     const { id } = useParams()
 
+    const [editing, setEditing] = useState(false)
+
     useEffect(() => {
         props.fetchUser(id)
-        props.getFollowing(id)
+        setEditUser(props.user)
     }, [])
+
+    const [editUser, setEditUser] = useState({
+        username: '',
+        bio: 'bio'
+    })
+
+    const handleChanges = e => {
+        setEditUser({
+            ...editUser,
+            [e.target.name]: e.target.value
+        })
+    }
 
     return (
         <>
-            <h1>Profile</h1>
-            <h1>{props.user.username}</h1>
             <Link to='/following'>Following</Link>
             <Link to='/explore'>Explore</Link>
+            <h1>Profile</h1>
+            <h1>{props.user.username}</h1>
+            {!editing ?
+                <div>
+                    <button onClick={() => setEditing(!editing)}>edit</button>
+                </div> :
+                <div>
+                    <button onClick={() => setEditing(!editing)}>done</button>
+                </div>
+            }
+            {props.user.bio == null || editing == true ? 
+                <>
+                    <input
+                        id='bio'
+                        type='text'
+                        name='bio'
+                        value={editUser.bio || props.user.bio}
+                        placeholder={props.user.bio}
+                    />
+                </> :
+                <>
+                    <p>{props.user.bio}</p>
+                </>
+            }
+            {props.user.username == null || editing == true? 
+                <>
+                    <input
+                        id='username'
+                        type='text'
+                        name='username'
+                        value={editUser.username}
+                        placeholder={props.user.username}
+                        onChange={handleChanges}
+                    />
+                </> :
+                <> 
+                    <p>{props.user.username}</p>
+                </>
+            }
         </>
     )
 }
---
+++ src/components/user/UserProfile.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { } from '../../actions'
+import { connect } from 'react-redux'
+import { getDefaultNormalizer } from '@testing-library/react'
+import axiosWithAuth from '../../utils/axiosWithAuth'
+import { useParams, Link } from 'react-router-dom'
+
+const UserProfile = props => {
+    const { id } = useParams()
+    const [currentUser, setCurrentUser] = useState({})
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get(`/api/users/${id}`)
+                .then(res => {
+                    console.log(res.data, props.user.id)
+                    setCurrentUser(res.data)
+                })
+    }, [])
+
+    return (
+        <>
+            <Link to={`/profile/${props.user.id}`}>Profile</Link>
+            <Link to='/following'>Following</Link>
+            <Link to='/explore'>Explore</Link>
+            <h1>usr profile</h1>
+            <h1>{currentUser.username}</h1>
+        </>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoading: state.isLoading,
+        error: state.error,
+        user: state.user,
+        users: state.users,
+        following: state.following
+    }
+}
+
+export default connect(mapStateToProps, { })(UserProfile)
