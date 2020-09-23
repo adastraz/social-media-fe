0f4e1f32738ec0e1d53e7a2399b20d71beede816
adastraz <tylergosox@gmail.com>
2020-09-23 17:07:41 -0400

    deployment

--- src/actions/index.js
+++ src/actions/index.js
@@ -15,7 +15,7 @@ export const FETCHING_SUCCESS_USERLIKES = 'FETCHING_SUCCESS_USERLIKES'
 export const login = creds => dispatch => {
     dispatch ({ type: FETCHING_START })
     axios
-        .post('http://localhost:3300/api/auth/login', creds)
+        .post('https://social-media-clone1.herokuapp.com/api/auth/login', creds)
             .then(res => {
                 dispatch ({ type: FETCHING_SUCCESS_LOGIN, payload: res.data.user })
                 localStorage.setItem('token', res.data.token)
@@ -28,12 +28,12 @@ export const login = creds => dispatch => {
 export const register = creds => dispatch => {
     dispatch({ type: FETCHING_START })
     axios
-        .post('http://localhost:3300/api/auth/register', creds)
+        .post('https://social-media-clone1.herokuapp.com/api/auth/register', creds)
             .then(res => {
                 dispatch({ type: FETCHING_SUCCESS })
                 dispatch({ type: FETCHING_START })
                 axios 
-                    .post('http://localhost:3300/api/auth/login', creds)
+                    .post('https://social-media-clone1.herokuapp.com/api/auth/login', creds)
                         .then(res => {
                             dispatch ({ type: FETCHING_SUCCESS_LOGIN, payload: res.data.user })
                             localStorage.setItem('token', res.data.token)
