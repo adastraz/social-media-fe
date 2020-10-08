c9b5fbe3dbf2325917deb0268bb027f739844704
adastraz <tylergosox@gmail.com>
2020-10-08 13:58:39 -0400

    fix window.reload on like

--- src/utils/axiosWithAuth.js
+++ src/utils/axiosWithAuth.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosWithAuth = () => {
     return axios.create({
-        baseURL: 'http://localhost:3300/',
+        baseURL: 'https://socialclone1.herokuapp.com/',
         headers: {
             Authorization: localStorage.getItem('token')
         }
